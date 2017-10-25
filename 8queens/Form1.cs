ac92a5668fdf44b77c734f4df14a117a39589333
MATHIEU\Mathieu <mathieufelipe@gmail.com>
2017-10-25 20:58:37 -0200

    First Commit
    8 queens parallel made in Windows Form C#

---
+++ 8queens/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8queens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            this.listBox1.Items.Add("Teste");
+            this.listBox1.Items.Add("Teste1");
+            this.listBox1.Items.Add("Teste2");
+            this.listBox1.Items.Add("Teste3");
+            this.listBox1.Items.Add("Teste4");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string currentItem = this.listBox1.SelectedItem.ToString();
+
+            Result result = new Result();
+            result.Text = result.Text + " " + currentItem;
+            result.Show();
+        }
+    }
+}
